769f9be22e0fe25afcc21cb314a9f799db92bc0c
Benjamin Newman <nlb9991@gmail.com>
2015-12-12 11:42:26 -0500

    This commit includes the geojson files for no-fly zones and a python parser that can identify if a give longitude and latitude are a no fly zone

---
+++ drone-feedback-master/readme.md
@@ -0,0 +1,21 @@
+Drone no fly zones - Feedback
+=============================
+
+#### Add your source to the map
+
+To add a data source to the no-fly map [mapbox.com/drone/no-fly](https://www.mapbox.com/drone/no-fly/#5/38.637/-96.500). Open a ticket with the following information:
+
+* Location
+* Source
+* Reason
+* Dataset, don't have a dataset? Make it with [geojson.io](http://geojson.io/)
+
+[[![](https://farm4.staticflickr.com/3866/14534145510_5a063623f4_h.jpg)](https://www.mapbox.com/drone/no-fly/#5/38.637/-96.500)](https://www.mapbox.com/drone/no-fly/#5/38.637/-96.500)
+
+
+#### Current Data Sources
+
+See `/sources` folder.
+
+#### Drone flying Octocat
+![](https://i.cloudup.com/MvxLclz-PM-3000x3000.jpeg)
---
+++ geoparser/geomjsonparser.py
@@ -0,0 +1,27 @@
+import json
+from shapely.geometry import shape, Point
+
+data = []
+with open("../drone-feedback-master/sources/geojson/5_mile_airport.geojson", "r") as map:
+	data.append(json.load(map))
+with open("../drone-feedback-master/sources/geojson/us_military.geojson", "r") as map:
+	data.append(json.load(map))
+with open("drone-feedback-master/sources/geojson/us_national_park.geojson". "r") as map:
+	data.append(json.load(map))
+	
+
+
+def canfly(coordinate):
+	coordinate = Point(coordinate)
+	for map in data:
+		for feature in map['features']:
+			polygon = shape(feature['geometry'])
+			if polygon.contains(coordinate):
+				print "You are too close to %s to fly a drone" %feature['properties']['name']
+				return False;
+			if feature['properties']['name'][:6].lower() == 'john f':
+				print "You are far enough from %s" %feature['properties']['name']
+				print polygon
+			
+	print "You can fly"
+	return True
